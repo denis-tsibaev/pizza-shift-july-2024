ae6a3a04dbc7653f0e1e52437837c0b164780a94
denis-tsibaev <denis.tsibaev@gmail.com>
2024-07-01 15:26:17 +0700

    Создал репозиторий

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Components/Header.jsx";
+import Footer from "./Components/Footer.jsx";
+import Home from "./Pages/Home.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Home />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
